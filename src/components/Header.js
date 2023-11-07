f3d522ff9fdc07205fe5292051a2c5b47bfb4070
Aftab Ahmed <ibaaftab12@gmail.com>
2023-11-08 01:41:14 +0500

    create basic template for react application

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Header() {
-return (
-<div> 
-     
-</div>
-)
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Row className="align-items-center ">
+          <Col>
+            <Navbar.Brand href="#home" className='logo'>SafeConvo</Navbar.Brand>
+          </Col>
+          <Col className="d-flex">
+            <Form className="d-flex">
+              <FormControl type="text" placeholder="Search" className=" Searchbar"/>
+              <Button variant="outline-success ">Search</Button>
+            </Form>
+          </Col>
+        </Row>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link href="#login" className="ps-5 " >Invite Friends</Nav.Link>
+            <Nav.Link href="#login" className="ps-5 ms-3 loginbutton"  >Login</Nav.Link>
+            <Nav.Link href="#signup" className="ps-5" >Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
